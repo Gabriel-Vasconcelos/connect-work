a419e3fc8828403a22f149e37ec3f3940512eec9
gabriel-vasconcelos <gabrieltotal6@gmail.com>
2024-09-05 21:56:55 -0300

    WIP

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,12 +1,17 @@
 import { LoginForm } from "@/components/LoginForm"
+import Image from "next/image";
 
 const LoginPage = () => {
   return (
-    <main>
-      <section>
+    <main className="site--container grid grid-cols-2 gap-x-32 my-20">
+      <section className="">
         <LoginForm />
       </section>
       <section>
+        <div className="flex flex-col items-center justify-center gap-y-10 bg-sky-300 p-14 px-16 rounded-2xl">
+          <Image src="/assets/Saly-10.png" alt="Imagem" width={423} height={423} className="" />
+          <h2 className="heading--2 font-bold text-sky-950">Bem vindo de volta!</h2>
+        </div>
       </section>
     </main>
   )
--- src/components/LoginForm/LoginForm.tsx
+++ src/components/LoginForm/LoginForm.tsx
@@ -5,13 +5,12 @@ import { LoginFormData } from "./types";
 import { Label } from "../ui/label";
 import { Input } from "../ui/input";
 import { Button } from "../ui/button";
+import Link from "next/link";
+import { FaApple, FaFacebook, FaGoogle } from "react-icons/fa";
+import { FcGoogle } from "react-icons/fc";
 
 export const LoginForm = () => {
-  const { handleSubmit, register, formState: { errors } } = useForm<LoginFormData>(
-    {
-      mode: "onSubmit"
-    }
-  );
+  const { handleSubmit, register, formState: { errors } } = useForm<LoginFormData>();
 
   const onSubmit: SubmitHandler<LoginFormData> = (data: LoginFormData) => {
 
@@ -22,22 +21,45 @@ export const LoginForm = () => {
   }
 
   return (
-    <form onSubmit={handleSubmit(onSubmit, onError)} className="">
-      <div>
-        <Label>Email</Label>
-        <Input {...register("email", { required: "Preencha o campo E-Mail" })} type="email" placeholder="Digitei seu E-mail" />
-        {errors.email && (
-          <span>{errors.email.message}</span>
-        )}
+    <form onSubmit={handleSubmit(onSubmit, onError)} className="grid">
+      <h1 className="text-4xl mx-auto font-bold">Login</h1>
+      <p className="text-lg mt-2 mb-8 flex justify-center gap-x-1">
+        <span className="opacity-60">Não possui uma conta? </span>
+        <Link href="/register" className="opacity-60 text-sky-600 w-fit lg:hover:opacity-100" title="cadastre-se">Cadastre-se aqui!</Link>
+      </p>
+      <div className="grid gap-4">
+        <div className="grid gap-2">
+          <Label htmlFor="email">Email</Label>
+          <Input id="email" {...register("email", { required: "Preencha o campo E-Mail" })} type="email" placeholder="Digitei seu E-mail" />
+          <p className="error--message">
+            {errors.email && errors.email.message}
+          </p>
+        </div>
+        <div className="grid gap-2">
+          <Link
+            href="/forgot-password"
+            className="ml-auto text-sm underline"
+          >
+            Esqueceu a senha?
+          </Link>
+          <Label htmlFor="password">Password</Label>
+          <Input id="password" {...register("password", { required: "Preencha o campo de Senha" })} type="password" placeholder="Digitei sua Senha" />
+          <p className="error--message">{errors.password && errors.password.message}</p>
+        </div>
       </div>
-      <div>
-        <Label>Password</Label>
-        <Input {...register("password", { required: "Preencha o campo de Senha" })} type="password" placeholder="Digitei sua Senha" />
-        {errors.password && (
-          <span>{errors.password.message}</span>
-        )}
+      <Button size="lg" className="mt-10 font-semibold">Entrar</Button>
+      <div className="mt-10">
+        <div className="grid grid-cols-[1fr_auto_1fr] items-center gap-x-5">
+          <hr />
+          <p >ou continue com</p>
+          <hr />
+        </div>
+        <div className="grid grid-cols-3 gap-x-4 mt-5">
+          <Button size="lg" variant="secondary" className="py-8 group"><FaFacebook color="blue" className="lg:group-hover:size-9" size="28" /></Button>
+          <Button size="lg" variant="secondary" className="py-8 group"><FcGoogle size="28" className="lg:group-hover:size-9" /></Button>
+          <Button size="lg" variant="secondary" className="py-8 group"><FaApple size="28" className="lg:group-hover:size-9" /></Button>
+        </div>
       </div>
-      <Button>Entrar</Button>
     </form>
   )
 }
